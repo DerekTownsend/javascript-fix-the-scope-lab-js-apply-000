445969e95d4a91ea07cb83904ae5b5ecefd46764
Derek Townsend <derekjavon2@gmail.com>
2019-02-26 02:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function myAnimal() {
 }
 
 
-animal = 'dog'
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal=cat
     return animal
 }
 
